30899d9af83ec33dd54ee936ae4591b530eaa869
oushesh <oushesh@gmail.com>
2023-06-16 15:02:49 +0400

    Landing_page_body

--- Frontend/components/Langing_Page_Body.tsx
+++ Frontend/components/Langing_Page_Body.tsx
@@ -129,6 +129,35 @@ export default function LandingPageBody()
     * No cards required for Trial
   </div>
 
+  <form onSubmit={handleFormSubmit}>
+                <input
+                  type="text"
+                  name="short_url"
+                  placeholder="Short URL"
+                  className="text-xl flex items-center p-4 rounded-full cursor-pointer bg-white hover:bg-slate-200 w-full text-gray-700"
+                  value={shortUrl}
+                  onChange={handleShortUrlChange}
+                />
+                <input
+                  type="text"
+                  name="long_url"
+                  placeholder="Long URL"
+                  className="text-xl flex items-center p-4 rounded-full cursor-pointer bg-white hover:bg-slate-200 w-full text-gray-700"
+                  value={longUrl}
+                  onChange={handleLongUrlChange}
+                />
+                <button type="submit" className="justify-center flex text-xl w-full px-4 py-2 text-zinc-200 bg-zinc-900 border-[1px] border-zinc-700 focus:bg-slate-200 rounded-tl-full rounded-bl-full rounded-br-full rounded-tr-full focus:outline-none focus:ring-[1px] focus:ring-sky-500 placeholder:text-zinc-400">
+                  <Image
+                    src="/icons/calendar.svg"
+                    width={30}
+                    height={30}
+                    className=""
+                    alt={''}
+                  />
+                  Request Demo
+                </button>
+              </form>
+
   <div className="mt-20 space-y-1">
         <h1 className="text-4xl font-bold">
           Productivity 
