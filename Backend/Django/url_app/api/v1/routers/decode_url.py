5eb077a1e5da874c86d8418d90877ab66414ce3d
oushesh <oushesh@gmail.com>
2023-06-19 11:44:11 +0400

    encode_decode with in momoery cache

--- Backend/Django/url_app/api/v1/routers/decode_url.py
+++ Backend/Django/url_app/api/v1/routers/decode_url.py
@@ -1,12 +1,21 @@
 import os
 from dotenv import load_dotenv
-
 from ninja import Router
 router = Router()
 
+from django.core.cache import cache
+from django.http import JsonResponse
+
+
 #Read the collections
 @router.get("/decode_url")
-def decode_url(short_url: str,url_mapping:dict):
+def decode_url(request,short_url: str):
+    assert (isinstance(short_url,str))
+
+    # Access the cached dictionary
+    url_mapping = cache.get("url_mapping",{})
+    #Example dict:
+    url_mapping = {"abcd":"https://www.google.com"}
     for key, value in url_mapping.items():
         if key.startswith(short_url[:6]):
             return {"long_url": value}
--- Backend/Django/url_app/api/v1/routers/encode_url.py
+++ Backend/Django/url_app/api/v1/routers/encode_url.py
@@ -6,6 +6,8 @@ from collections import Counter
 from pydantic import BaseModel
 from hashlib import md5
 from itertools import count
+from django.http import JsonResponse
+from django.core.cache import cache
 
 counter = count()
 router = Router()
@@ -17,12 +19,17 @@ class URLOutput(BaseModel):
     short_url: str
 
 @router.get("/encode_url")
-def encode_url(request,url_input: str,url_mapping:dict):
-    #long_url = url_input.url
+def encode_url(request,url_input: str):
+    # Access and modify the cache
+    url_mapping = cache.get("url_mapping",{})
+
     long_url = url_input
     md5_hash = md5(long_url.encode()).hexdigest()
     short_url = f'{md5_hash[:6]}{next(counter)}'
     url_mapping[short_url] = long_url
-    print (url_input)
-    print (url_mapping)
+
+    #add the data back to the cache
+    cache.set('url_mapping',url_mapping)
     return {"short_url": short_url}
+
+##Keep the values in Django cache backend --> I added the settings
