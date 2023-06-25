0ccee0176a03cd7d42b026921094df805f553243
oushesh <oushesh@gmail.com>
2023-06-25 11:17:08 +0400

    updated: Backend and Test

--- Backend/FastAPI/test/test_index.py
+++ Backend/FastAPI/test/test_index.py
@@ -1,4 +1,5 @@
 import pytest
 
-from src.msg.data import data
-from src.index import encode_url, decode_url
+from FastAPI.src.msg.data import data
+from FastAPI.src.index import encode_url, decode_url
+
