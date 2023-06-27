934577f8c6de3d84dd3605e964ff7a840e40d5ee
oushesh <oushesh@gmail.com>
2023-06-27 11:45:59 +0400

    test_encoder_decoder

--- Backend/Django/test/test_encoder_decoder.py
+++ Backend/Django/test/test_encoder_decoder.py
@@ -5,12 +5,12 @@ current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 sys.path.append(parent)
 
-from  url_app.api.v1.schemas.message import data
-from src.index import encode_url, decode_url
+#from  url_app.api.v1.schemas.message import data
+#from src.index import encode_url, decode_url
 
 class TestURLShortenerFail:
 
-    encoded_URL = "http://127.0.0.1:8000/api/encode_url"
+    encoded_URL = "'http://127.0.0.1:8000/api/encode_url"
     decoded_URL = "http://127.0.0.1:8000/api/decode_url"
 
     @pytest.mark.asyncio
